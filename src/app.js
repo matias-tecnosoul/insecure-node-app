0a651ce2660a5fe38c6c1df5cd39cfabf738312a
matias-tecnosoul <matias@tecnosoul.com.ar>
2025-06-16 10:31:12 -0300

    remove extraVar

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 const API_KEY = 'ghp_a1b2c3d4e5f6g7h8i9j0klmnopqrstuvwx12';
-const extraVar = 'Esta variable ya no se usa, pero alguien olvidó borrarla!'
 
 app.use(express.json());
 
